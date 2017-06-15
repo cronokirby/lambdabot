d2f3a81fd61c344b296be3b4661df2ee7cacc7fd
cronokirby <cronokirby@gmail.com>
2017-06-15 13:16:30 +0200

    makes casing not matter for role assignment

--- lib/commands/roles.ex
+++ lib/commands/roles.ex
@@ -9,44 +9,61 @@ defmodule Lambdabot.Commands.Roles do
 
   @lang_roles Application.fetch_env!(:lambdabot, :lang_roles)
 
-  Cogs.set_parser(:role, &List.wrap/1)
-  Cogs.def role(requested) do
-    roles = String.split(requested)
-    edit_roles(message, roles, &Client.add_role/3, fn good, bad ->
-      "I've given you the following roles: ```#{inspect good}```\n" <>
-      case bad do
-        [] -> ""
-        x  -> "The following roles don't seem to be valid: ```#{inspect x}```"
-      end
+  def format_list([], formatter) do
+    ""
+  end
+  def format_list(l, formatter) do
+    formatter.(inspect(l))
+  end
+
+  Cogs.set_parser(:role, &[String.split(&1)])
+  Cogs.def role(roles) do
+    edit_roles(message, roles, &Client.add_role/3, fn g, f, b ->
+      format_list(g,
+        &"I've given you the following roles ```#{&1}```"
+      ) <>
+      format_list(f,
+       &"The following exist, but I couldn't give them to you ```#{&1}```"
+      ) <>
+      format_list(b,
+        &"The following roles don't seem to be valid: ```#{&1}```"
+      )
     end)
   end
 
-  Cogs.set_parser(:remrole, &List.wrap/1)
-  Cogs.def remrole(requested) do
-    roles = String.split(requested)
-    edit_roles(message, roles, &Client.remove_role/3, fn good, bad ->
-      "I've removed the following roles: ```#{inspect good}```\n" <>
-      case bad do
-        [] -> ""
-        x  -> "The following roles don't seem to be valid: ```#{inspect x}```"
-      end
+  Cogs.set_parser(:remrole, &[String.split(&1)])
+  Cogs.def remrole(roles) do
+    edit_roles(message, roles, &Client.remove_role/3, fn g, f, b ->
+      format_list(g,
+        &"I've removed the following roles ```#{&1}```"
+      ) <>
+      format_list(f,
+       &"The following exist, but I couldn't give them to you ```#{&1}```"
+      ) <>
+      format_list(b,
+        &"The following roles don't seem to be valid: ```#{&1}```"
+      )
     end)
   end
 
   def edit_roles(message, roles, action, formatter) do
     {:ok, guild_id} = Cogs.guild_id()
     sender = message.author.id
-    {good, bad} = Enum.reduce(roles, {[], []}, fn x, {l, r} ->
-      with {:ok, role} <- Map.fetch(@lang_roles, x),
-           {:ok, nil} <- action.(guild_id, sender, role)
-      do
-        {[x | l], r}
-      else
-        _ -> {l, [x | r]}
-      end
+    init = %{good: [], failed: [], bad: []}
+    result = roles
+      |> Enum.map(&String.downcase/1)
+      |> Enum.reduce(init, fn x, acc ->
+        with {:ok, role} <- Map.fetch(@lang_roles, String.downcase(x)),
+             {:ok, nil} <- action.(guild_id, sender, role)
+        do
+          Map.update(acc, :good, [], &([x | &1]))
+        else
+          :error -> Map.update(acc, :bad, [], &([x | &1]))
+          _      -> Map.update(acc, :failed, [], &([x | &1]))
+        end
     end)
     %Embed{color: 0xce67bf}
-    |> description(formatter.(good, bad))
+    |> description(formatter.(result.good, result.failed, result.bad))
     |> Embed.send
   end
 end
